f6c639035b50d3c805b51792e1e09be794398bf2
Nikitt22 <87071043+Nikitt22@users.noreply.github.com>
2023-10-18 12:18:18 +0800

    commit dependency injection

--- src/main/java/org/springframework/TestSpring.java
+++ src/main/java/org/springframework/TestSpring.java
@@ -4,8 +4,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("MusicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        //Music music = context.getBean("MusicBean", Music.class);
+       // MusicPlayer musicPlayer = new MusicPlayer(music);
+        // Используем Dependency Injection
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
